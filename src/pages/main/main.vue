38571a1aa6422e91f41c210ae7f5912824dc42ed
ang.li <twelve1997@126.com>
2022-09-14 18:23:41 +0800

    feat(element-push组件注册)

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     {{ $store.state.name }}
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 
